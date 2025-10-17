c17ec2a60a2bedf1f0c86cc1883d765462bea1cd
Husniahmed10 <husniahmed07@gmail.com>
2025-10-17 17:33:50 +0530

    Backend_initialization

--- src/graphs/graph_builder.py
+++ src/graphs/graph_builder.py
@@ -23,5 +23,19 @@ class GraphBuilder:
         self.graph.add_edge(START, "title_creation")
         self.graph.add_edge("title_creation", "content_generation")
         self.graph.add_edge("content_generation", END)
+        
+        return self.graph
+    
+    def setup_graph(self, usecase):
+        if usecase=="topic":
+            self.build_topic_graph()
 
         return self.graph.compile()
+    
+## Below code is for the langsmith langgraph studio
+
+llm = OpenLLM().get_llm()
+
+## Get the graph
+graph_builder = GraphBuilder(llm)
+graph = graph_builder.build_topic_graph().compile()
